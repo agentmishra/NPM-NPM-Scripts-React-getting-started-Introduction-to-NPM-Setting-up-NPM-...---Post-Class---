5cab3cb4f637bb9eb26d7964ef0a45db402dd19a
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-02-08 23:43:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,128 +1,61 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 import data from './data.js';
 
-const weatherAPI = {
-  key: "a64d07a306972de8e9f8c8267a9cc2c0",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
-
-const newsAPI = {
-  key: "1321740c80c3874a09a04e802b81c02a",
-  base: "https://gnews.io/api/v4/"
-}
 
 
-function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({main:{temp: ''},name: '', sys: {country: ''}, weather: [{main: ''}]});
-  const [news, setNews] = useState(data);
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${weatherAPI.base}weather?q=${query}&units=metric&APPID=${weatherAPI.key}`)
-        .then(res => {
-          if (res.ok) 
-          return res.json();
-          else{
-            setQuery('');
-            throw new Error('Invalid location/city/country');
-          }
-        })
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        })
-        .catch((error) => {
-          console.log(error)
-        });
+const App = () => {
+  const [weather, setWeather] = useState(JSON.stringify({}))
+  const [news, setNews] = useState(JSON.stringify({ title: "", description: "" }))
+  const [lat, setlati] = useState("")
+  const [long, setlongi] = useState("")
+  const [newlocation, setnewlocatio] = useState("")
+
+  useEffect(() => {
+    if (lat !== "" && long !== "")
+    // also runs if one of lat or long is updated
+    {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${'da05b6e0fe788db7dcdc421c300214c1'}`, { method: "GET" }).then(e => e.json()).then(e => {
+        setWeather(JSON.stringify({ temperature: e.main.temp, location: e.coord }))
+      })
+      fetch(`https://gnews.io/api/v4/search?q=example&token=410db42779f25b2d81028050efe65502&max=5`).then(e => e.json()).then(e => {
+        // for(int )  
+        setNews(JSON.stringify({ key: "1", title: e.articles[0].title, description: e.articles[0].description }))
+      })
     }
+
+  }, [lat, long])
+
+  const findFromCity = (e) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${e}&appid=${'da05b6e0fe788db7dcdc421c300214c1'}`, { method: "GET" }).then(e => e.json()).then(e => {
+      setWeather(JSON.stringify({ key: "2", temperature: e.main.temp, location: e.coord }))
+    }).catch(e => {
+      setWeather(JSON.stringify({ key: "3", message: "invalid location" }))
+    })
   }
-  useEffect(() =>{
-    const fetchWeather = async () =>{
-      if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(async (position) =>{
-          // get user's location
-          let pos = {
-            lat: position.coords.latitude,
-            lon: position.coords.longitude, 
-          }
-          // get whether of user's location
-          await fetch(`${weatherAPI.base}weather?lat=${pos.lat}&lon=${pos.lon}&units=metric&appid=${weatherAPI.key}`)
-          .then(res => {
-            if (res.ok)
-              return res.json();
-            else
-              throw new Error('Invalid location/city/country');
-          })
-          .then(result => {
-            setWeather(result);
-            console.log(result);
-          })
-          .catch(error => console.log(error));
-        });
-      }
-    }
-    const fetchNews = async () =>{
-      if(news.articles.length === 1){
-        await fetch(`${newsAPI.base}top-headlines?&lang=hi&token=${newsAPI.key}`)
-          .then(function (response) {
-              if(response.ok)
-              return response.json();
-            else
-              throw new Error('Something went wrong')
-          })
-          .then(function (data) {
-            setNews(data);
-            console.log(data);
-          })
-          .catch(error => console.log(error));
-        }
-    }
-   fetchWeather();
-   fetchNews();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((e) => {
+      console.log("Got the Location", e);
+      setlati(e.coords.latitude)
+
+      setlongi(e.coords.longitude)
+    }, (e) => console.log('facing issue in getting the coordinates', e), {});
   }, [])
-  
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
+  const inputHandler = (e) => {
+    if (e.key === "Enter") {
+      findFromCity(document.getElementsByTagName('input')[0].value)
+    }
   }
-  
+
   return (
-    <div className="App">
-    <div className='weather-info'>
-    <div className="weather-search-box">
-      <input className="search" type="text" onChange={e => setQuery(e.target.value)} placeholder="search.." value={query} onKeyPress={search}></input>
-    </div>
-    <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-      <div className="weather-box">
-        <div className="temp">{Math.round(weather.main.temp)}°c</div>
-        <div className="weather">{weather.weather[0].main}</div>
-      </div>
-      </div>
-      <div className='news-info'>
-          {news.articles.map((article) => {
-            return <article className='article' key={article.title}>
-            <a href={article.url}><h3>{article.title}</h3></a>
-              <p>{article.description}</p>
-              <img src={article.image} width='200px' height='200px'/>
-            </article>
-          })}
-      </div>
-    </div>
-  );
+    <>
+      <input onKeyDown={inputHandler} />
+      {weather}
+      {news}
+    </>)
 }
+
 
 export default App;
-
